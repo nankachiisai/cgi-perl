d74074a962eaa5584f79214d2ed476f254abfd48
yasuda0102 <webnuac@gmail.com>
2025-12-10 21:01:12 +0900

    とりあえず作った

---
+++ cgi-bin/index.cgi
@@ -0,0 +1,9 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+
+print "Content-type: text/html\n\n";
+print "<html><head><title>CGI Test</title></head><body>";
+print "<h1>Hello, World!</h1>";
+print "</body></html>";
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/cgi"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	scriptDir := "cgi-bin"
+
+	// ディレクトリがなければ作成
+	if _, err := os.Stat(scriptDir); os.IsNotExist(err) {
+		_ = os.Mkdir(scriptDir, 0755)
+	}
+
+	http.HandleFunc("/cgi-bin/", func(w http.ResponseWriter, r *http.Request) {
+		// URLパスからスクリプト名を取得 (test.pl)
+		scriptName := r.URL.Path[len("/cgi-bin/"):]
+		if scriptName == "" {
+			http.Error(w, "Script name required", http.StatusBadRequest)
+			return
+		}
+
+		cwd, _ := os.Getwd()
+		scriptPath := filepath.Join(cwd, scriptDir, scriptName)
+
+		if _, err := os.Stat(scriptPath); os.IsNotExist(err) {
+			http.Error(w, "Script not found", http.StatusNotFound)
+			return
+		}
+
+		// CGIハンドラー設定
+		handler := cgi.Handler{
+			Path: scriptPath,
+			Dir:  scriptDir,
+			Env:  []string{
+				// Perlに必要な環境変数があればここに追加
+				// "PERL5LIB=/my/lib/path", 
+			},
+		}
+
+		handler.ServeHTTP(w, r)
+	})
+
+	port := ":8080"
+	log.Printf("Starting CGI Server on http://localhost%s", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
